533bc1ca1d20a849715c2a2415e3f7e7ac5f9069
connorjohnson6 <70149458+connorjohnson6@users.noreply.github.com>
2023-09-15 17:30:15 -0400

    Completed check for if user has an account - google

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,17 +1,30 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
-        //options for google strat
-        callbackURL: '/auth/google/redirect',
+        // options for google strategy
         clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret    
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
-        //passport callback function
-        
+        // check if user already exists in our own db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have this user
+                console.log('user is: ', currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
 
+                });
+            }
+        });
     })
-
-)
+);
