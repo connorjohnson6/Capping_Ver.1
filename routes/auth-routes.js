d7751109dcdfed0c64afd0ce5adc1419841f211b
connorjohnson6 <70149458+connorjohnson6@users.noreply.github.com>
2023-09-15 18:22:04 -0400

    user auth corrected and working

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-//auth login
-router.get('/login', (req,res) => {
-    res.render('login');
+// auth login
+router.get('/login', (req, res) => {
+    res.render('login', { user: req.user });
 });
 
-//auth logout
-router.get('/logout', (req,res) => {
-    //hand with passport
-    res.send('logging out')
+// auth logout
+router.get('/logout', (req, res) => {
+    // handle with passport
+    res.send('logging out');
 });
 
-
-//auth with google
+// auth with google+
 router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
-//callback route for google to redirect to
-router.get('/google/redirect', passport.authenticate('google'),(req, res) => {
-    res.send("this is the callback URI")
+// callback route for google to redirect to
+// hand control to passport to use code to grab profile info
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile');
 });
 
 module.exports = router;
