095a40edc6f11ca5d15f6e7c3a11f61b61a80537
ahanys1 <alexanderhanys@gmail.com>
2023-10-17 22:21:08 -0400

    climate IQ interface integrated

--- private/climateiqInterface.js
+++ private/climateiqInterface.js
@@ -2,7 +2,16 @@ const API_KEY = "ZCCX5Y2TNMMSEVHC68G1BG2P5J0T";
 const travelURL = "https://beta4.api.climatiq.io/travel/distance";
 //https://www.climatiq.io/docs/api-reference/travel
 
-function getDrivingData(start, end, carType = "petrol", carSize = "medium"){
+
+/**
+ * Quereys ClimateIQ for driving data. 
+ * @param {string} start - the origin city. Ex: "Boston, MA" 
+ * @param {string} end - the destination city. this and the start are a query; they search a database for best match.
+ * @param {string} [carType] - car types: "petrol", "diesel", "hybrid", "plugin_hybrid", "battery". Defaults to average car.
+ * @param {string} [carSize] - car sizes: "small" (below 1.7L) "medium" (1.7L-2.0L) "large" (above 2.0L). Defaults to average car.
+ * @returns {JSON} The resulting query of the API. important Properties: co2e (double), co2e_unit (string), distance_km (double)
+ */
+function getDrivingData(start, end, carType = "average", carSize = "average"){ 
     let output;
     let data = {
         origin: {
@@ -41,14 +50,92 @@ function getDrivingData(start, end, carType = "petrol", carSize = "medium"){
 
 }
 
-function getFlightData(start, end){ 
-    
+/**
+ * Quereys ClimateIQ for flight data. 
+ * @param {string} start - the origin airport IATA code. Ex: "EWR" (Newark)
+ * @param {string} end - the destination airport IATA code. Ex: "DEN" (Denver)
+ * @param {string} [flightClass] - Flight classes: "economy", "first", "buisness". Defaults to average.
+ * @returns {JSON} The resulting query of the API. important Properties: co2e (double), co2e_unit (string), distance_km (int)
+ */
+function getFlightData(start, end, flightClass = "average"){ 
+    let output;
+    let data = {
+        origin: {
+            iata: start
+        },
+        destination: {
+            iata: end
+        },
+        travel_mode: "air",
+        air_details: {
+            class: flightClass
+        }
+    };
+    (async () => {
+        const fetch = (await import('node-fetch')).default;
+        
+        const response = await fetch(travelURL, {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+      
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log(responseData);
+          output = responseData;
+        } else {
+          console.error('Error:', response.status, response.statusText);
+        }
+      })();
+    return output;
 
 }
 
-function getTrainData(origin, end){
-
+/**
+ * Quereys ClimateIQ for Train data. 
+ * @param {string} start - the origin city. Ex: "Boston, MA" 
+ * @param {string} end - the destination city. this and the start are a query; they search a database for best match.
+ * @returns {JSON} The resulting query of the API. important Properties: co2e (double), co2e_unit (string), distance_km (double)
+ */
+function getTrainData(start, end){
+    let output;
+    let data = {
+        origin: {
+            query: start
+        },
+        destination: {
+            query: end
+        },
+        travel_mode: "rail",
+    };
+    (async () => {
+        const fetch = (await import('node-fetch')).default;
+        
+        const response = await fetch(travelURL, {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+      
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log(responseData);
+          output = responseData;
+        } else {
+          console.error('Error:', response.status, response.statusText);
+        }
+      })();
+    return output;
 }
 
-//getFlightData("EWR", "DEN");
-//getDrivingData("New York, New York", "Westport, NY", "petrol", "large");
+//example Calls:
+//getDrivingData("Poughkeepsie, NY", "Montreal, Canada", "petrol", "medium");
+//getFlightData("EWR", "DEN", "economy");
+//getTrainData("New York, New York", "Boston, MA");
