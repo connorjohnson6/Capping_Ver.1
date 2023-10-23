26cfd8e451b342c337f9882a1e0cc35d24a40e69
connorjohnson6 <70149458+connorjohnson6@users.noreply.github.com>
2023-10-23 15:18:19 -0400

    n

--- landing_Page/config/keys.js
+++ landing_Page/config/keys.js
@@ -1,17 +1,1 @@
-//add this file to .gitignore
-//tell everyone
 
-module.exports = {
-    google:{
-        clientID : '497863077790-3ddbb1hnon1p1rn9psrttsaflvckml64.apps.googleusercontent.com',
-        clientSecret : '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'
-    },
-
-    mongodb:{
-        dbURI: 'mongodb+srv://maristcapping2024:!C4pp1ng!.DB@cluster0.ojekpug.mongodb.net/?retryWrites=true&w=majority'
-    },
-
-    session: {
-        cookieKey: 'uofbuoiqibiuqbueqwnjimomoicebnhcwcrtcwe'
-    }
-};
