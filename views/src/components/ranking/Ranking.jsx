21f4135133fc4b5e86aa580cf54a89437dc1ece2
Matthew-OMalley <90004260+Matthew-OMalley@users.noreply.github.com>
2023-12-09 04:34:01 +0000

    final comments

--- views/src/components/ranking/Ranking.jsx
+++ views/src/components/ranking/Ranking.jsx
@@ -1,43 +1,57 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { VStack, HStack, StackDivider, Text } from '@chakra-ui/react'; // Adjust imports based on your setup
+import { VStack, HStack, StackDivider, Text } from '@chakra-ui/react';
 import './ranking.css';
 import { AuthContext } from "../../context/AuthContext";
 
 export default function Ranking({ ranking }) {
+  // State to hold user goals and emissions data
   const [usersGoals, setUsersGoals] = useState([]);
+  
+  // Get the current user from the AuthContext
   const { user: currentUser } = useContext(AuthContext);
 
+  // Effect hook to fetch user goals and emissions data
   useEffect(() => {
+    // Function to fetch user goals and emissions data
     const fetchUsersGoalsAndEmissions = async () => {
+      // Construct the combined URL for the API endpoint
       const combinedUrl = `${process.env.REACT_APP_BACKEND_URL}/api/users/allUsersGoalsAndEmissions`;
 
       try {
+        // Make a GET request to the API
         const response = await axios.get(combinedUrl);
+        // Get the combined data from the response
         const combinedData = response.data;
 
+        // Sort the users based on their progress percentage
         const sortedUsers = combinedData.sort((a, b) => {
           const progressA = a.goal > 0 ? (a.totalCo2E / a.goal) * 100 : 0;
           const progressB = b.goal > 0 ? (b.totalCo2E / b.goal) * 100 : 0;
           return progressB - progressA;
         });
 
+        // Update the state with the sorted user data
         setUsersGoals(sortedUsers);
       } catch (error) {
+        // Log an error if there's an issue with the API request
         console.error('Error fetching combined users goals and emissions:', error);
       }
     };
 
+    // Fetch data only if a user is logged in
     if (currentUser && currentUser._id) {
       fetchUsersGoalsAndEmissions();
     }
   }, [currentUser]);
 
+  // Function to calculate progress percentage
   const calculateProgress = (goal, emissions) => {
     const progress = goal > 0 ? (emissions / goal) * 100 : 0;
     return progress;
   };
 
+  // JSX rendering
   return (
     <VStack
       divider={<StackDivider borderColor="gray.200" />}
@@ -45,12 +59,17 @@ export default function Ranking({ ranking }) {
       align="stretch"
       className="leaderboard-container"
     >
+      {/* Leaderboard title */}
       <Text fontWeight="bold" fontSize="2xl" alignSelf="center" p={4}>
         Leaderboard
       </Text>
+      
+      {/* Map through each user and display their information */}
       {usersGoals.map((user, index) => {
+        // Calculate progress percentage for each user
         const progress = calculateProgress(user.goal, user.totalCo2E);
 
+        // Render user information
         return (
           <HStack key={index} p={4} justify="space-between" align="center" className="leaderboard-entry">
             <VStack align="start" spacing={1} width="100%">
@@ -61,6 +80,7 @@ export default function Ranking({ ranking }) {
               <Text fontSize="xs" color="gray.600">
                 Progress: {progress.toFixed(0)}%
               </Text>
+              {/* Progress bar */}
               <div className="progress-container">
                 <div className="progress-bar" style={{ width: `${progress}%` }}></div>
               </div>
--- views/src/pages/green/Green.jsx
+++ views/src/pages/green/Green.jsx
@@ -4,74 +4,74 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Rightbar from "../../components/rightbar/Rightbar";
 import "./green.css";
 
+// External stylesheet link for custom fonts
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
+// Component for the "Green" page
 export default function Green() {
+  // Data for CO2 emissions from various activities
+  const emissionsData = [
+    {
+      id: 1,
+      activity: "Driving a Car",
+      emissions: 2.3, // in kilograms of CO2 per mile
+    },
+    {
+      id: 2,
+      activity: "Using a Gas Lawnmower",
+      emissions: 3.5, // in kilograms of CO2 per hour
+    },
+    {
+      id: 3,
+      activity: "Air Travel (short-haul flight)",
+      emissions: 0.2, // in kilograms of CO2 per passenger-mile
+    },
+    // Add more activities and emissions data as needed
+    {
+      id: 4,
+      activity: "Heating a Home with Natural Gas",
+      emissions: 4.2, // in kilograms of CO2 per hour
+    },
+    {
+      id: 5,
+      activity: "Eating Beef",
+      emissions: 13.3, // in kilograms of CO2 per pound
+    },
+    {
+      id: 6,
+      activity: "Using a Gas-Powered Generator",
+      emissions: 6.8, // in kilograms of CO2 per hour
+    },
+  ];
 
-const emissionsData = [
-  {
-    id: 1,
-    activity: "Driving a Car",
-    emissions: 2.3, // in kilograms of CO2 per mile
-  },
-  {
-    id: 2,
-    activity: "Using a Gas Lawnmower",
-    emissions: 3.5, // in kilograms of CO2 per hour
-  },
-  {
-    id: 3,
-    activity: "Air Travel (short-haul flight)",
-    emissions: 0.2, // in kilograms of CO2 per passenger-mile
-  },
-  // Add more activities and emissions data as needed
-  {
-    id: 4,
-    activity: "Heating a Home with Natural Gas",
-    emissions: 4.2, // in kilograms of CO2 per hour
-  },
-  {
-    id: 5,
-    activity: "Eating Beef",
-    emissions: 13.3, // in kilograms of CO2 per pound
-  },
-  {
-    id: 6,
-    activity: "Using a Gas-Powered Generator",
-    emissions: 6.8, // in kilograms of CO2 per hour
-  },
-  // Add more activities and emissions data as needed
-];
+  // Data for environmentally friendly alternatives
+  const alternativesData = [
+    {
+      id: 1,
+      alternative: "Carpooling or using public transportation",
+    },
+    {
+      id: 2,
+      alternative: "Switching to an electric or manual lawnmower",
+    },
+    {
+      id: 3,
+      alternative: "Opting for eco-friendly transportation or reducing air travel",
+    },
+    {
+      id: 4,
+      alternative: "Switching to energy-efficient heating methods",
+    },
+    {
+      id: 5,
+      alternative: "Choosing plant-based diet options",
+    },
+    {
+      id: 6,
+      alternative: "Using renewable energy sources instead of generators",
+    },
+  ];
 
-const alternativesData = [
-  {
-    id: 1,
-    alternative: "Carpooling or using public transportation",
-  },
-  {
-    id: 2,
-    alternative: "Switching to an electric or manual lawnmower",
-  },
-  {
-    id: 3,
-    alternative: "Opting for eco-friendly transportation or reducing air travel",
-  },
-  {
-    id: 4,
-    alternative: "Switching to energy-efficient heating methods",
-  },
-  {
-    id: 5,
-    alternative: "Choosing plant-based diet options",
-  },
-  {
-    id: 6,
-    alternative: "Using renewable energy sources instead of generators",
-  },
-  // Add more alternative data as needed
-];
-
-
+  // Function to render CO2 emissions data
   const renderEmissions = () => {
     return emissionsData.map((item) => (
       <div key={item.id} className="emissions-item">
@@ -81,6 +81,7 @@ const alternativesData = [
     ));
   };
 
+  // Function to render environmentally friendly alternatives
   const renderAlternatives = () => {
     return (
       <div className="alternatives">
@@ -90,25 +91,32 @@ const alternativesData = [
             <li key={item.id} style={{ marginBottom: '13px' }}>{item.alternative}</li>
           ))}
         </ul>
+        {/* Material Symbols Outlined icon for recycling */}
         <span class="material-symbols-outlined large-icon">recycling </span>
       </div>
     );
   };
 
+  // JSX rendering
   return (
     <>
+      {/* Top navigation bar */}
       <Topbar />
       <div className="homeContainer">
+        {/* Sidebar for navigation */}
         <Sidebar />
         <div className="mainContent">
+          {/* Container for CO2 emissions information */}
           <div className="emissions-container">
             <h1>CO2 Emissions from Activities</h1>
             {renderEmissions()}
+            {/* Material Symbols Outlined icon for drama (possibly representing filter options) */}
             <span class="material-symbols-outlined large-icon">filter_drama</span>
           </div>
+          {/* Render environmentally friendly alternatives */}
           {renderAlternatives()}
-          
         </div>
+        {/* Sidebar on the right side of the page */}
         <Rightbar pageType="green" />
       </div>
     </>
