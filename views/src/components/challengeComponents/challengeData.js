b91a6b4278b6b149c98bd6f717686dea5d9e2d21
andrewberp <123495166+andrewberp@users.noreply.github.com>
2023-12-11 15:31:06 -0500

    Update challengeData.js

--- views/src/components/challengeComponents/challengeData.js
+++ views/src/components/challengeComponents/challengeData.js
@@ -12,17 +12,17 @@ const challengesData = {
     availableChallenges: [
       {
         id: 1,
-        name: 'Reduce December\'s transportation emissions by 8 Tons, or 30%',
+        name: 'Reduce December\'s transportation emissions by 30KG, or 30%',
         endDate: '12/31/23',
       },
       {
         id: 2,
-        name: 'Achieve a monthly emission total below 10 Tons',
+        name: 'Achieve a monthly emission total below 60KG',
         endDate: '12/31/23',
       },
       {
         id: 3,
-        name: 'Achieve the perfect day: < 1lb of emissions',
+        name: 'Achieve the perfect day: < 1KG of emissions',
         endDate: '12/31/23',
       },
       // Add more challenges as needed
